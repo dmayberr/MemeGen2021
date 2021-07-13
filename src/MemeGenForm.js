2e04870da664df8ad9dca2846492e151c64540c7
Darrell Mayberry <dmayberr@gmail.com>
2021-07-13 05:59:48 -0400

    Added Bootswatch theme (Darkly) and created brand text.

---
+++ src/MemeGenForm.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react'
+import './MemeGenForm.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import "./bootstrap.min.css"
+
+const MemeGenForm = () => {
+    
+    return (
+        <Container>
+            <Navbar expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand>                    
+                    <h1 className="display-2">MemeGen 2021</h1>
+                </Navbar.Brand>
+            </Navbar> 
+            
+        </Container>
+    )
+}
+
+export default MemeGenForm
