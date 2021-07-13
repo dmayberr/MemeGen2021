cedb6bb1b135def6d8a124e74b188ff04b0fa680
Darrell Mayberry <dmayberr@gmail.com>
2021-07-13 06:35:44 -0400

    Created navbar with now function yet

--- src/MemeGenForm.js
+++ src/MemeGenForm.js
@@ -1,20 +1,50 @@
 // import React, { useState } from 'react'
 import './MemeGenForm.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import "./bootstrap.min.css"
+
+import './bootstrap.min.css'
+
+
 
 const MemeGenForm = () => {
     
     return (
-        <Container>
-            <Navbar expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand>                    
-                    <h1 className="display-2">MemeGen 2021</h1>
-                </Navbar.Brand>
-            </Navbar> 
-            
-        </Container>
+        // <Container>
+        //     <Navbar expand="lg" bg="dark" variant="dark">
+        //         <Navbar.Brand>
+        //             <p className="display-2">MemeGen 2021</p>
+        //         </Navbar.Brand>
+        //     </Navbar>
+
+        // </Container>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#home">
+                    MemeGen 2021
+                </a>
+
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home">
+                                Create New Meme
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home">
+                                Pricing
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home">
+                                About
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
